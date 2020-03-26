6ed791b54f6d78333f1a30eab7aed02dac04b88d
puneet-sg25 <60170576+puneet-sg25@users.noreply.github.com>
2020-03-26 13:06:05 +0530

    Add files via upload

---
+++ mouseoperations/Spec/MouseMoveSpec.ts
@@ -0,0 +1,13 @@
+import { browser, element, by} from 'protractor'
+describe('Protractor Typescript Demo', function() {
+	browser.ignoreSynchronization = true; // for non-angular websites
+	browser.manage().window().maximize()
+	it('Mouse Operations', function() {
+		// set implicit time to 30 seconds
+		browser.manage().timeouts().implicitlyWait(30000);
+
+		browser.get("https://chercher.tech/practice/popups")
+		// mouse hover on a submenu
+		browser.actions().mouseMove(element(by.id("sub-menu"))).perform()
+	});
+});
---
+++ mouseoperations/Spec/js/protractor_config_jasmine.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    directConnect: true,
+    // Capabilities to be passed to the webdriver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    // Framework to use. Jasmine is recommended.
+    framework: 'jasmine',
+    // Spec patterns are relative to the current working directory when
+    // protractor is called.
+    spec: [
+        'MouseMoveSpec.js'
+    ],
+};
