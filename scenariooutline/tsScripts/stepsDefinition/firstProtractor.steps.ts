49bf9e5c23d98bf3fd0f613a59f9965c485d577b
puneet-sg25 <60170576+puneet-sg25@users.noreply.github.com>
2020-04-09 19:06:37 +0530

    Updated with when function

--- scenariooutline/tsScripts/stepsDefinition/firstProtractor.steps.ts
+++ scenariooutline/tsScripts/stepsDefinition/firstProtractor.steps.ts
@@ -1,5 +1,5 @@
 import {Given,When,Then, After, AfterAll, BeforeAll, Before} from 'cucumber'
-import {browser,element,by} from 'protractor'
+import {browser,element,by,protractor} from 'protractor'
 
 BeforeAll(async function(){
   await browser.get('http://juliemr.github.io/protractor-demo/');
@@ -15,6 +15,21 @@ Given('Super Calculator Website is opened browser', async function () {
      await console.log("[2] Entered first number.");
    });
   });
+
+  When('Select {word} operator from dropdown', async  function (operatorString) {
+    let operatorCSS:string = "";
+    switch(operatorString){
+      case "Multiply": operatorCSS = "option:nth-child(4)";
+      break;
+      case "Modulo": operatorCSS = "option:nth-child(3)";
+      break;
+      case "Divide": operatorCSS = "option:nth-child(2)";
+      break;
+    }
+      await element(by.model('operator')).element(by.css(operatorCSS)).click().then(async function(){
+          console.log("[3] Operator Selected");
+      });
+    });
 
   When('Enter number {int} in Second Box', async function (secondNum) {
     await element(by.model('second')).sendKeys(secondNum).then(async function(){
@@ -32,8 +47,15 @@ Given('Super Calculator Website is opened browser', async function () {
   Then('Answer Displayed is {int}', async function (answerNum){
      await console.log("[6] Answer Displayed");
   });
-
+  
   After(async function () {
-        let screenShot= await browser.takeScreenshot();
-        this.attach(screenShot, "image/png");
-});
+    browser.sleep(2000);
+    //browser.driver.executeScript("document.body.style.zoom='60%'");
+    let screenShot= await browser.takeScreenshot();
+    this.attach(screenShot, "image/png");  
+  });
+
+
+
+
+
